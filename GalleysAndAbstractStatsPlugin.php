19a44add740b65ecb52912519f61c795dc5174d8
yves <yves@lepidus.com.br>
2023-12-21 18:02:51 -0400

    Fisrt implementation of galleys and abstract stats.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#722
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Thiago <thiago@lepidus.com.br>

--- GalleysAndAbstractStatsPlugin.php
+++ GalleysAndAbstractStatsPlugin.php
@@ -4,6 +4,11 @@ namespace APP\plugins\generic\galleysAndAbstractStats;
 
 use PKP\plugins\GenericPlugin;
 use PKP\plugins\Hook;
+use APP\core\Application;
+use APP\core\Request;
+use APP\core\Services;
+use APP\facades\Repo;
+use APP\template\TemplateManager;
 
 class GalleysAndAbstractStatsPlugin extends GenericPlugin
 {
@@ -12,13 +17,19 @@ class GalleysAndAbstractStatsPlugin extends GenericPlugin
         $success = parent::register($category, $path, $mainContextId);
         if ($success && $this->getEnabled($mainContextId)) {
             Hook::add('Templates::Article::Details', [$this, 'viewDataStatement']);
+            Hook::add('TemplateManager::display', function ($hookName, $args) {
+                $request = Application::get()->getRequest();
+                $templateMgr = TemplateManager::getManager($request);
+                $pluginFullPath = $request->getBaseUrl() . '/' . $this->getPluginPath();
+                $templateMgr->addStyleSheet('GalleysAndAbstractStats', $pluginFullPath . '/styles/index.css');
+            });
         }
         return $success;
     }
 
     public function getDisplayName()
     {
-        return 'Galleys And Abstract Stats Plugin';
+        return __('plugins.generic.galleysAndAbstractStats.name');
     }
 
     /**
@@ -26,15 +37,31 @@ class GalleysAndAbstractStatsPlugin extends GenericPlugin
      */
     public function getDescription()
     {
-        return 'Galleys And Abstract Stats Plugin';
+        return __('plugins.generic.galleysAndAbstractStats.description');
     }
 
     public function viewDataStatement(string $hookName, array $params): bool
     {
         $templateMgr = &$params[1];
         $output = &$params[2];
+        $request = Application::get()->getRequest();
+        $publication = $templateMgr->getTemplateVars('publication');
+        $submissionId = $publication->getData('submissionId');
+        $contextId = $request->getContext()->getId();
+        $statsService = Services::get('publicationStats');
+        $metricsByType = $statsService->getTotalsByType($submissionId, $contextId, null, null);
+        $galleys = Repo::galley()->getCollector()
+                ->filterByPublicationIds(['publicationIds' => $publication->getId()])
+                ->getMany();
+
+        $galleysViews = [];
+        foreach ($galleys as $galley) {
+            $galleysViews[] = [$galley->getGalleyLabel(), $galley->getViews()];
+        }
+
         $templateMgr->assign([
-            'galleysAndAbstractStats' => 'Made with ❤ by the Lepidus Tecnologia',
+            'abstractViews' => $metricsByType['abstract'],
+            'galleysViews' => $galleysViews,
         ]);
 
         $output .= $templateMgr->fetch($this->getTemplateResource('index.tpl'));
--- templates/index.tpl
+++ templates/index.tpl
@@ -1,3 +1,14 @@
-<div class="pkp_block block_madeBy">
-  {$galleysAndAbstractStats|escape}
+<div class="item views">
+  <div class="label">{translate key="plugins.generic.galleysAndAbstractStats.index.label"}</div>
+  <div class="value">
+    <ul style="list-style: none; padding: 0; margin: 0;">
+      <li style="padding: 5px 0;">{translate key="article.abstract"} <span id="viewsCount">{$abstractViews}</span></li>
+      {if count($galleysViews) > 0}
+        {foreach from=$galleysViews item=galley}
+          <li style="padding: 5px 0;">{$galley[0]} <span class="task_count" id="viewsCount">{$galley[1]}<span></li>
+        {/foreach}
+      {/if}
+    </ul>
+    <sub>{translate key="plugins.generic.galleysAndAbstractStats.index.viewsAndDownloadsInformation"}</sub>
+  </div>
 </div>
